4ece076207360fef19e76d33d3942833302a946c
Ana Geremias <anageremiias@gmail.com>
2025-02-04 16:59:47 -0300

    Inclusão Database

---
+++ ListaTarefaAPI/Data/ListaTarefasContext.cs
@@ -0,0 +1,17 @@
+﻿using ListaTarefaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListaTarefaAPI.Data
+{
+    public class ListaTarefasContext : DbContext
+    {
+        public ListaTarefasContext
+            (DbContextOptions<ListaTarefasContext> options) : base(options)
+        {
+        }
+        public DbSet<Tarefa> Tarefas { get; set; }
+
+        public DbSet<TipoTarefa> TipoTarefa { get; set; }
+
+    }
+}
